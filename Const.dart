778c4ad7c50f46ff6559c7040b27c735bb4907b9
Ketan Inamdar <91153995+inamdarketan@users.noreply.github.com>
2022-03-22 21:01:24 +0530

    Add files via upload

---
+++ Const.dart
@@ -0,0 +1,15 @@
+
+// Const Keyword
+main() {
+  const aconstnum = 0;
+  const aconstbool = true;
+  const aconststring = 'a constant string';
+
+  print(aconstnum);
+  print(aconstbool);
+  print(aconststring);
+
+  print(aconstnum.runtimeType);
+  print(aconstbool.runtimeType);
+  print(aconststring.runtimeType);
+}
---
+++ TypeConversion.dart
@@ -0,0 +1,22 @@
+// Type Conversion
+main() {
+
+  var one = int.parse('1');
+  assert(one == 1);
+
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+
+}
+
+// main() {
+
+//   String oneAsString = 1.toString();
+//   assert(oneAsString == '1');
+
+//   String piAsString = 3.14159.toStringAsFixed(2);
+//   assert(piAsString == '3.14');
+
+// }
+
+
